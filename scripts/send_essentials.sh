8cbe0d9ee92831ba93ba595f00f8a2fe6290b7d5
Deni Bertovic <deni@denibertovic.com>
2016-10-20 15:22:54 +0200

    Adds basic scripts

---
+++ scripts/send_essentials.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied."
+    echo "Usage: ./send_essentials.sh 192.168.0.123"
+    exit 1;
+fi
+
+to=$1
+
+rsync -avz --progress projects $to:~
+rsync -avz --progress work $to:~
+
