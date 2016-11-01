b38b7cb718599925b8fe083ca5676895b1a7fdf6
Deni Bertovic <deni@denibertovic.com>
2016-11-01 12:30:13 +0100

    Makes trayer fit nicely with xmobar

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -32,7 +32,7 @@ myStartupHook = do
     spawnOnce "gnome-keyring-daemon --start"
     spawnOnce "xmodmap ~/.Xmodmap"
     spawnOnce "gnome-settings-daemon"
-    spawnOnce "trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 10 --transparent true --tint 0x000 --heighttype pixel --height 32"
+    spawnOnce "trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 20 --transparent true --tint 0x222222 --heighttype pixel --height 36"
     spawnOnce "parcellite"
     spawnOnce "synclient TouchpadOff=1"
     spawnOnce "redshift -c /home/deni/dotfiles/redshift.conf"
