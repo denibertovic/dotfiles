96712c6ef028c98ed87994d820d879dbe18ee9ce
Deni Bertovic <deni@denibertovic.com>
2018-07-03 11:54:09 +0200

    Small monitor manipulation script tweaks

--- scripts/monitors_laptop.sh
+++ scripts/monitors_laptop.sh
@@ -4,7 +4,7 @@ killall trayer
 # Workaround for dunst not refreshing it's geometry and positioning after
 # switching back to a single monitor
 killall dunst
-xrandr --output DP-2-2 --off --output DP-2-1 --off --auto
+xrandr --output eDP-1 --auto --primary --output DP-2-2 --off --output DP-2-1 --off
 nohup trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 20 --transparent true --tint 0x222222 --height 33 > /tmp/nohup.out 2>&1 &
 rm -rf ~/.xmonad/xmonad.state
 
--- scripts/monitors_work.sh
+++ scripts/monitors_work.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 killall trayer
-xrandr --output eDP-1 --auto --primary --below DP-2-2 --output DP-2-2 --right-of DP-2-1 --auto --output DP-2-1 --rotate left --auto
+xrandr --output eDP-1 --auto --primary --below DP-2-2 --output DP-2-2 --auto --right-of DP-2-1 --output DP-2-1 --auto --rotate left
 nohup trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 20 --transparent true --tint 0x222222 --height 33 --monitor 2 > /tmp/nohup.out 2>&1 &
 rm -rf ~/.xmonad/xmonad.state
 
