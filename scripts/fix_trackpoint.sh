b92a8aa2573bf1dbd8229d03bae5a1af83f6403c
Deni Bertovic <deni@denibertovic.com>
2020-03-17 16:39:34 +0100

    Upadates trackpoint script

--- scripts/fix_trackpoint.sh
+++ scripts/fix_trackpoint.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-echo 180 >  /sys/devices/platform/i8042/serio1/serio2/sensitivity
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root."
+  exit
+fi
+
+echo 200 >  /sys/devices/platform/i8042/serio1/serio2/sensitivity
 echo 160 >  /sys/devices/platform/i8042/serio1/serio2/speed
