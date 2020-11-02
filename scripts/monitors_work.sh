10d1662f11cfb7bd57a6ff69d7492479af810562
Deni Bertovic <deni@denibertovic.com>
2020-11-02 12:41:25 +0100

    Comments in work monitors helper scripts

--- scripts/monitors_work.sh
+++ scripts/monitors_work.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+# Scren name     | Resolution | Size | DPI | Scaling          |
+# ============================================================|
+# Laptop: eDP1   | 2560x1440  | 14"  | 210 | 210/210 = 1x1    |
+# Dell-1 DP-2-2  | 2560x1440  | 27"  | 109 | 210/109 = 1.926  |
+# Dell-2: DP-2-1 | 1920x1080  | 23"  | 96  | 210/96  = 2.187  |
+# ============================================================|
+
 killall trayer
-xrandr --output eDP-1 --auto --primary --below DP-2-2 --output DP-2-2 --auto --right-of DP-2-1 --output DP-2-1 --auto --rotate left
-nohup trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 20 --transparent true --tint 0x222222 --height 33 --monitor 2 > /tmp/nohup.out 2>&1 &
+xrandr --output eDP-1 --auto --right-of DP-2-2 --output DP-2-2 --auto --primary --right-of DP-2-1 --output DP-2-1 --auto --rotate left
+nohup trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 20 --transparent true --tint 0x222222 --height 33 --monitor 1 > /tmp/nohup.out 2>&1 &
 rm -rf ~/.xmonad/xmonad.state
 
