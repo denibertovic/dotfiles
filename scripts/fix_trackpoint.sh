ebcfcc7fe6ec88dc9d45faddefbf0950d9ade757
Deni Bertovic <deni@denibertovic.com>
2022-09-08 14:54:55 +0200

    Speed up trackpoint

--- scripts/fix_trackpoint.sh
+++ scripts/fix_trackpoint.sh
@@ -5,5 +5,5 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-echo 200 >  /sys/devices/platform/i8042/serio1/serio2/sensitivity
-echo 160 >  /sys/devices/platform/i8042/serio1/serio2/speed
+echo 220 >  /sys/devices/platform/i8042/serio1/serio2/sensitivity
+echo 220 >  /sys/devices/platform/i8042/serio1/serio2/speed
