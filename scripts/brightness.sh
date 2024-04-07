62552c962563d17dd0cff9082909913ca8c0de10
Deni Bertovic <deni@denibertovic.com>
2024-04-07 11:48:01 +0200

    Fix intel_backlight/brigtness permissions

--- scripts/brightness.sh
+++ scripts/brightness.sh
@@ -23,5 +23,4 @@ new_brightness_p=$(($old_brightness_p $1))
 new_brightness=$(( $max_brightness * $new_brightness_p / 100  ))
 
 # set the new brightness value
-chmod 666 $handler"brightness"
 echo $new_brightness > $handler"brightness"
