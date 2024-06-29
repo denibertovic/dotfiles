eef62cc199d038b6581ff11a59db6f76ee0f9e77
Deni Bertovic <deni@denibertovic.com>
2024-06-29 20:23:12 +0200

    nix: add local bin in path

--- nix/machines/kanta/configuration.nix
+++ nix/machines/kanta/configuration.nix
@@ -315,6 +315,8 @@ in
 
   ];
 
+  environment.localBinInPath = true;
+
   # For this to work I had to log into dropbox.com in the browser first.
   # Then do "dropbox start -i" from the terminal. This installed the daemon and
   # prompted me in the browser to connect my device to my account.
