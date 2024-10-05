a8573afad9d0142c90afcad042cc15faef2ce987
Deni Bertovic <deni@denibertovic.com>
2024-10-05 11:42:22 +0200

    nix dropbox notes

--- nix/home/dropbox.nix
+++ nix/home/dropbox.nix
@@ -1,5 +1,10 @@
 { config, pkgs, lib, ... }:
 {
+  # NOTES: First log into the default browser
+  # Then start the service and let it download .dropbox-dist
+  # Connect via browser once the tab opens
+  # if the service fails just remove the .dropbox-dist folder (not the .dropbox folder)
+  # and restart
   services.mydropbox = {
     enable = true;
   };
