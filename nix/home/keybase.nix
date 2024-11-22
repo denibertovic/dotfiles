a863d0761edf96bfa5ecb509a59082ce7f80d87b
Deni Bertovic <deni@denibertovic.com>
2024-11-22 16:30:27 +0100

    Keybase fix/workaround

--- nix/home/keybase.nix
+++ nix/home/keybase.nix
@@ -2,4 +2,6 @@
 {
   services.keybase.enable = true;
   services.kbfs.enable = true;
+  # workaround: https://github.com/nix-community/home-manager/pull/5655
+  systemd.user.services.kbfs.Service.PrivateTmp = lib.mkForce false;
 }
