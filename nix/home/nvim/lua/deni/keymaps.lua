1b08d698e7fa5d83400a710957316b35dec88ced
Deni Bertovic <deni@denibertovic.com>
2024-12-19 19:30:20 +0100

    neovim: add toggle for lsp warnings

--- nix/home/nvim/lua/deni/keymaps.lua
+++ nix/home/nvim/lua/deni/keymaps.lua
@@ -2,3 +2,13 @@
 vim.keymap.set("n", "<C-j>", vim.cmd.bprev)
 vim.keymap.set("n", "<C-k>", vim.cmd.bnext)
 vim.keymap.set({ "n", "v", "o" }, "<C-d>", vim.cmd.bd)
+
+vim.keymap.set("n", "<leader>dt", function()
+   -- this will be deprecated in favor of is_enabled()
+   -- so the logic will need to be switched
+   if vim.diagnostic.is_disabled() then
+     vim.diagnostic.enable()
+   else
+     vim.diagnostic.disable()
+   end
+end)
