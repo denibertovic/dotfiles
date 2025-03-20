fba84731c65c3b5a4a382498b11dcd6e9a37b334
Deni Bertovic <deni@denibertovic.com>
2025-03-20 16:07:31 +0100

    nvim: trying new keymaps

--- nix/home/nvim/lua/deni/keymaps.lua
+++ nix/home/nvim/lua/deni/keymaps.lua
@@ -1,7 +1,9 @@
 -- buffer manipulation
 vim.keymap.set("n", "<C-j>", vim.cmd.bprev)
 vim.keymap.set("n", "<C-k>", vim.cmd.bnext)
-vim.keymap.set({ "n", "v", "o" }, "<C-d>", vim.cmd.bd)
+vim.keymap.set({ "n", "v", "o" }, "D", vim.cmd.bd)
+-- vim.keymap.set({ "n", "v", "o" }, "<leader>x", "<Esc>:bprevious<bar>bdelete #<Return>")
+-- vim.keymap.set({ "n", "v", "o" }, "<leader>X", ":%bd!|e#<cr>")
 
 vim.keymap.set("n", "<leader>dt", function()
    -- this will be deprecated in favor of is_enabled()
