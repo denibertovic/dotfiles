f843a3fe3db6a78c98fb4e19b28161c78cc554a8
Deni Bertovic <deni@denibertovic.com>
2026-01-18 22:45:36 +0100

    disable avante in nvim

--- nix/home/nvim/lua/deni/plugins/ai.lua
+++ nix/home/nvim/lua/deni/plugins/ai.lua
@@ -1,15 +1,15 @@
-require("avante_lib").load()
-require("avante").setup({
-	-- add any opts here
-	-- for example
-	providers = {
-		openai = {
-			endpoint = "https://api.anthropic.com/v1/messages",
-			model = "claude-3-5-sonnet-20241022", -- your desired model
-			timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
-			temperature = 0,
-			max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-			--reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
-		},
-	},
-})
+-- require("avante_lib").load()
+-- require("avante").setup({
+-- 	-- add any opts here
+-- 	-- for example
+-- 	providers = {
+-- 		openai = {
+-- 			endpoint = "https://api.anthropic.com/v1/messages",
+-- 			model = "claude-3-5-sonnet-20241022", -- your desired model
+-- 			timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
+-- 			temperature = 0,
+-- 			max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+-- 			--reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+-- 		},
+-- 	},
+-- })
