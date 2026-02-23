add886429df85a27eb8675812823af548f929d0a
Deni Bertovic <deni@denibertovic.com>
2026-02-23 21:46:29 +0100

    Update scripts for current hardware
    - monitors_work.sh: disable third monitor
    - set_trackpoint_speed.sh: use Elan TrackPoint device name

--- scripts/set_trackpoint_speed.sh
+++ scripts/set_trackpoint_speed.sh
@@ -2,7 +2,8 @@
 #https://www.reddit.com/r/thinkpad/comments/z3gw6x/help_t480_trackpoint_is_awfully_slow_in_linux/
 #xinput set-prop 12 187 X 0 0 0 Y 0 0 0 1
 # xinput set-prop 11 187 2.2 0 0 0 2.2 0 0 0 1
-xinput set-prop "TPPS/2 IBM TrackPoint" 187 2.0 0 0 0 2.0 0 0 0 1
+# xinput set-prop "TPPS/2 IBM TrackPoint" 187 2.0 0 0 0 2.0 0 0 0 1
+xinput set-prop "TPPS/2 Elan TrackPoint" 187 2.0 0 0 0 2.0 0 0 0 1
 
 # https://www.reddit.com/r/thinkpad/comments/b5gqsv/change_trackpoint_speed_under_linux/
 # xinput --set-prop "TPPS/2 IBM TrackPoint" "libinput Accel Speed" 1.00
